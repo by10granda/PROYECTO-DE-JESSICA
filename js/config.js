5d293be7945098aff06af8036065ea2c0f25385d
Your Name <you@example.com>
2026-08-05 21:52:22 -0500

    Update Apps Script deployment URL

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.AppConfig = {
   appName: 'Recetas Jessica',
   spreadsheetId: '16dXfmZbyFqNHVDKPPetcVdRXSydX6oIMpOrRRY_8uac',
   // Pegue aquí la URL del Web App publicado desde Google Apps Script.
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzj6p44MRC_Y8HdfURtzwj6Scr8oGaCASMbzIFSOPiUfj63WUunq7OJYa3pZn5XHBXc/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbwqY3bus9FaArkO4DFPi8Jn8BdMgkPsvkJG5qHPCwHnKXfIG3xlm7IohEfXVdC6wV3Q/exec',
   localStorageKey: 'recetas-jessica-local-db-v1',
   doctorStorageKey: 'recetas-jessica-doctor-v1',
   defaultDoctor: {
