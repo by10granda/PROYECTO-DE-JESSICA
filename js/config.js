f960a0d540a0ac809d1d61faa6a9e57e4f35fac1
Your Name <you@example.com>
2026-08-05 20:46:17 -0500

    Configure Apps Script endpoint

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.AppConfig = {
   appName: 'Recetas Jessica',
   spreadsheetId: '16dXfmZbyFqNHVDKPPetcVdRXSydX6oIMpOrRRY_8uac',
   // Pegue aquí la URL del Web App publicado desde Google Apps Script.
-  appsScriptUrl: '',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzj6p44MRC_Y8HdfURtzwj6Scr8oGaCASMbzIFSOPiUfj63WUunq7OJYa3pZn5XHBXc/exec',
   localStorageKey: 'recetas-jessica-local-db-v1',
   doctorStorageKey: 'recetas-jessica-doctor-v1',
   defaultDoctor: {
